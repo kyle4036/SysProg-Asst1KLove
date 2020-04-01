1fbe40f8246d7e1d5f6514af476aa10059059c11
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-01 14:26:33 -0400

    Update Huffman.c

--- Huffman.c
+++ Huffman.c
@@ -7,3 +7,19 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <dirent.h>
+
+int buildCodeBook(char* fileName){
+  return 0;
+}
+
+int compress(char* fileName,char* codeBook){
+  return 0;
+}
+
+int decompress(char* fileName,char* codeBook){
+  return 0;
+}
+
+int recursive(char* pathName, char flag){
+  return 0;
+}
