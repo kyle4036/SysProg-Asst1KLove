c3b4269d0bfe44a73cd20f62406192565a155730
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-01 14:55:38 -0400

    Update fileCompressor.c

--- fileCompressor.c
+++ fileCompressor.c
@@ -11,6 +11,27 @@
 
 #include "Huffman.h"
 
+int validFlag(char* input);
+
 int main(int argc,char* argv[]){
+  if(argv[1] == "")
   return 0;
+}
+
+int validFlag(char* input){
+  if(strncmp(input, "-b")){
+    return 1;
+  }
+  else if(strncmp(input, "-c")){
+    return 1;
+  }
+  else if(strncmp(input, "-d")){
+    return 1;
+  }
+  else if(strncmp(input, "-R")){
+    return 2;
+  }
+  else{
+    return 0;
+  }
 }
