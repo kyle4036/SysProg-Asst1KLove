43aa9859b656ecdae00666a5b90fc067724e4a1b
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-01 22:40:24 -0400

    Update Huffman.h

--- Huffman.h
+++ Huffman.h
@@ -4,7 +4,7 @@
   int buildCodeBook(char* fileName);
   int compress(char* fileName,char* codeBook);
   int decompress(char* fileName,char* codeBook);
-  int recursive(char* pathName, char flag,char* codeBook);
+  int recursive(char* pathName, char* flag,char* codeBook);
 
   char* flagToString(char* flag);
 
