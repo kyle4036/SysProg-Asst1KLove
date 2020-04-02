8a385523903644d2bc1e17ba88faa2a83d4c4a20
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-02 14:06:29 -0400

    Update lList.h

--- lList.h
+++ lList.h
@@ -1,8 +1,8 @@
 #ifndef LLIST_H
 #define LLIST_H
 
-  typedef struct lList_{l_node* head;} lList;
   typedef struct l_node_{char* token;int freq;l_node_* next;l_node_* prev;} l_node;
+  typedef struct lList_{l_node* head;} lList;
 
   lList createList(char* token);
   void sortList(lList data);
