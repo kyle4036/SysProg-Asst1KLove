2ef9e27be0b577cb6d3568399084e5e33e9f537a
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-01 22:10:01 -0400

    setup basic user interface output code

--- Huffman.c
+++ Huffman.c
@@ -10,18 +10,54 @@
 #include <dirent.h>
 
 int buildCodeBook(char* fileName){
+  printf("Building codebook for %s!\n", fileName);
   return 0;
 }
 
 int compress(char* fileName,char* codeBook){
+  printf("Compressing %s using %s codebook!\n",fileName, codeBook);
   return 0;
 }
 
 int decompress(char* fileName,char* codeBook){
+  printf("Decompressing %s using %s codebook!\n",fileName, codeBook);
   return 0;
 }
 
 int recursive(char* pathName, char* flag, char* codeBook){
-  printf("solving recursively!");
+  char* action = flagToString(flag);
+  printf("Recursively %s directory %s!\n",action,pathName);
   return 0;
+}
+
+char* flagToString(char* flag){
+  int length;
+
+  char[] bString = "building codebook for";
+  char[] cString = "compressing";
+  char[] dString = "decompressing";
+
+  if(strncmp(input, "-b",3)){
+    length = strnlen(bString,30);
+    char* action = malloc(sizeof(char) * length);
+    strncpy(action,bString,length);
+    return action;
+  }
+  else if(strncmp(input, "-c",3)){
+    length = strnlen(cString,30);
+    char* action = malloc(sizeof(char) * length);
+    strncpy(action,cString,length);
+    return action;
+  }
+  else if(strncmp(input, "-d",3)){
+    length = strnlen(dString,30);
+    char* action = malloc(sizeof(char) * length);
+    strncpy(action,dString,length);
+    return action;
+  }
+  else{
+    printf("Error In flagToString():\n\t Bad flag input.\n");
+    exit(EXIT_FAILURE);
+    return 0;
+  }
 }
--- Huffman.h
+++ Huffman.h
@@ -1,7 +1,11 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
+
   int buildCodeBook(char* fileName);
   int compress(char* fileName,char* codeBook);
   int decompress(char* fileName,char* codeBook);
   int recursive(char* pathName, char flag);
+
+  char* flagToString(char* flag);
+
 #endif
