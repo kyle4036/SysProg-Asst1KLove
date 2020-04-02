0ce27fa5bd9412380f98b96985279449c2cae103
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-02 13:52:16 -0400

    creating lList libraries

---
+++ lList.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <dirent.h>
+
+#include "lList.h"
+
+lList createList(char* token);
+void sortList(lList data);
+void addToken(lList data,char* token);
---
+++ lList.h
@@ -0,0 +1,11 @@
+#ifndef LLIST_H
+#define LLIST_H
+
+  typedef struct lList_{l_node* head} lList;
+  typedef struct l_node_{char* token;int freq;l_node_* next;l_node_* prev} l_node;
+
+  lList createList(char* token);
+  void sortList(lList data);
+  void addToken(lList data,char* token);
+
+#endif
