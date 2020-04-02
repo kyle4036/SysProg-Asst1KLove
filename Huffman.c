445d211c68ccb4237714059438e287dba16f4085
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-01 22:16:39 -0400

    Update Huffman.c

--- Huffman.c
+++ Huffman.c
@@ -33,30 +33,30 @@ int recursive(char* pathName, char* flag, char* codeBook){
 char* flagToString(char* flag){
   int length;
 
-  char[] bString = "building codebook for";
-  char[] cString = "compressing";
-  char[] dString = "decompressing";
+  char bString[] = "building codebook for";
+  char cString[] = "compressing";
+  char dString[] = "decompressing";
 
-  if(strncmp(input, "-b",3)){
+  if(strncmp(flag, "-b",3)){
     length = strnlen(bString,30);
     char* action = malloc(sizeof(char) * length);
     strncpy(action,bString,length);
-    return action;
+    return (char*)action;
   }
-  else if(strncmp(input, "-c",3)){
+  else if(strncmp(flag, "-c",3)){
     length = strnlen(cString,30);
     char* action = malloc(sizeof(char) * length);
     strncpy(action,cString,length);
     return action;
   }
-  else if(strncmp(input, "-d",3)){
+  else if(strncmp(flag, "-d",3)){
     length = strnlen(dString,30);
     char* action = malloc(sizeof(char) * length);
     strncpy(action,dString,length);
     return action;
   }
   else{
-    printf("Error In flagToString():\n\t Bad flag input.\n");
+    printf("Error In flagToString():\n\t Bad flag.\n");
     exit(EXIT_FAILURE);
     return 0;
   }
