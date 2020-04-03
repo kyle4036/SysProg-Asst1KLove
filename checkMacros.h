a8e6bc3c270e90b0ba5ede6863637ee990b770bf
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-03 19:22:12 -0400

    Update checkMacros.h

--- checkMacros.h
+++ checkMacros.h
@@ -2,20 +2,22 @@
 #define CHECKMACROS_H
 
 #define CHECKREAD(x) do{\
-  if((x) == -1){\
-    printf("Line# %d:\tCannot Read File: ",(__LINE__)-1);\
-    switch(errno){\
-      case EINVAL: printf("File unsuitable for reading.\n"); exit(1);\
-      default : printf("Kernel or program error.\n"); exit(1);\
+    if((x) == -1){\
+        printf("In - %s\nFunction - %s\nLine# %d",__FILE__,__FUNC__,(__LINE__)-1);\
+        printf(":\n\tCannot Read File: ");\
+        switch(errno){\
+            case EINVAL: printf("File unsuitable for reading.\n"); exit(1);\
+            default : printf("Kernel or program error.\n"); exit(1);\
+        }\
     }\
-  }\
 }while(0)\
 
 #define CHECKMALLOC(x) do{\
-  if((x) == NULL){\
-    printf("Couldn't malloc() on line %d! Exiting...",(__LINE__)-1);\
-    exit(1);\
-  }\
+    if((x) == NULL){\
+        printf("In - %s\nFunction - %s\nLine# %d",__FILE__,__FUNC__,(__LINE__)-1);\
+        printf(":\n\tCouldn't malloc() on line %d! Exiting...",(__LINE__)-1);\
+        exit(1);\
+    }\
 }while(0)\
 
 #endif
