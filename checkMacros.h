541c3ba9e8576aac9e457264c5551a509e4ae881
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-03 00:50:25 -0400

    Create checkMacros.h

---
+++ checkMacros.h
@@ -0,0 +1,21 @@
+#ifndef CHECKMACROS_H
+#define CHECKMACROS_H
+
+#define CHECKREAD(x) do{\
+  if((x) == -1){\
+    printf("Line# %d:\tCannot Read File: ",(__LINE__)-1);\
+    switch(errno){\
+      case EINVAL: printf("File unsuitable for reading.\n"); exit(1);\
+      default : printf("Kernel or program error.\n"); exit(1);\
+    }\
+  }\
+}while(0)\
+
+#define CHECKMALLOC(x) do{\
+  if(x == NULL){\
+    printf("Couldn't malloc() on line %d! Exiting...",(__LINE__)-1);\
+    exit(1);\
+  }\
+}while(0)\
+
+#endif
