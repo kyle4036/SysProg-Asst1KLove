752a10c0558fb89445746b24060470efa34d3b91
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-03 19:08:06 -0400

    fixing tabbing: Huffman.h

--- Huffman.h
+++ Huffman.h
@@ -3,14 +3,14 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
-  int buildCodeBook(char* fileName, int fd);
-  int compress(char* fileName,char* codeBook);
-  int decompress(char* fileName,char* codeBook);
-  int recursive(char* pathName, char* flag,char* codeBook);
+    int buildCodeBook(char* fileName, int fd);
+    int compress(char* fileName,char* codeBook);
+    int decompress(char* fileName,char* codeBook);
+    int recursive(char* pathName, char* flag,char* codeBook);
 
-  lList* readInData(int fd);
-  void catString(char** stringO,int stringLen,char charTemp);
+    lList* readInData(int fd);
+    void catString(char** stringO,int stringLen,char charTemp);
 
-  char* flagToString(char* flag);
+    char* flagToString(char* flag);
 
 #endif
