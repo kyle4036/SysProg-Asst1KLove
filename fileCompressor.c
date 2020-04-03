bae9be78b8dc0a015a6fd63d5c21a6c6597091e2
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-03 18:58:11 -0400

    fixing tabbing:fileCompressor.c

--- fileCompressor.c
+++ fileCompressor.c
@@ -23,87 +23,87 @@ int improperName();
 
 int main(int argc,char* argv[]){
 
-  char* lastArg = argv[argc-1];
+    char* lastArg = argv[argc-1];
 
-  //check if the command line flags are valid
-  if(argc < 3 || argc > 5){
-    improperUsage();
-  }
-
-  //*****NOTE:: the final argument in recursive
-  //            may be a codeBook or a PathName,
-  //  need to add a check somewhere here
-  switch(validFlag(argv[1])){
-    case NOTGOOD:
-      improperUsage();
-      break;
-    case GOOD:
-      if(validFlag(argv[2])==GOOD){
+    //check if the command line flags are valid
+    if(argc < 3 || argc > 5){
         improperUsage();
-      }
-      if(validFlag(argv[2])==FLAGR){
-        recursive(argv[3], argv[1], lastArg);
-      }
-      branch(argv[1],argv[2], lastArg);
-      break;
-    case FLAGR:
-      if(validFlag(argv[2])!=GOOD){
-        improperUsage();
-      }
-      recursive(argv[3],argv[2],lastArg);
-      break;
-  }
+    }
 
-  return 0;
+    //*****NOTE:: the final argument in recursive
+    //            may be a codeBook or a PathName,
+    //  need to add a check somewhere here
+    switch(validFlag(argv[1])){
+        case NOTGOOD:
+            improperUsage();
+            break;
+        case GOOD:
+            if(validFlag(argv[2])==GOOD){
+                improperUsage();
+            }
+            if(validFlag(argv[2])==FLAGR){
+                recursive(argv[3], argv[1], lastArg);
+            }
+            branch(argv[1],argv[2], lastArg);
+            break;
+        case FLAGR:
+            if(validFlag(argv[2])!=GOOD){
+                improperUsage();
+            }
+            recursive(argv[3],argv[2],lastArg);
+            break;
+    }
+
+    return 0;
 }
 
 int validFlag(char* input){
-  if(!strncmp(input, "-b",3)){
-    return GOOD;
-  }
-  else if(!strncmp(input, "-c",3)){
-    return GOOD;
-  }
-  else if(!strncmp(input, "-d",3)){
-    return GOOD;
-  }
-  else if(!strncmp(input, "-R",3)){
-    return FLAGR;
-  }
-  else{
-    return 0;
-  }
+    if(!strncmp(input, "-b",3)){
+        return GOOD;
+    }
+    else if(!strncmp(input, "-c",3)){
+        return GOOD;
+    }
+    else if(!strncmp(input, "-d",3)){
+        return GOOD;
+    }
+    else if(!strncmp(input, "-R",3)){
+        return FLAGR;
+    }
+    else{
+        return 0;
+    }
 }
 
 //split off into the different functions based on the input
 int branch(char* flag, char* fileName, char* codeBook){
-
   //*****NOTE:: need to add checks for fileName and codeBook in compress and decompress
+    if(!strncmp(flag, "-b",3)){
+        int fd = open(fileName, O_RDONLY);
 
-  if(!strncmp(flag, "-b",3)){
-    int fd = open(fileName, O_RDONLY);
-    if(fd == -1){
-      improperName();
+        if(fd == -1)
+            improperName();
+
+        buildCodeBook(fileName,fd);
+        close(fd);
+      }
+
+    else if(!strncmp(flag, "-c",3)){
+        compress(fileName, codeBook);
     }
-    buildCodeBook(fileName,fd);
-    close(fd);
-  }
-  else if(!strncmp(flag, "-c",3)){
-    compress(fileName, codeBook);
-  }
-  else if(!strncmp(flag, "-d",3)){
-    decompress(fileName, codeBook);
-  }
-  return 0;
+    else if(!strncmp(flag, "-d",3)){
+        decompress(fileName, codeBook);
+    }
+    return 0;
 }
 
 int improperUsage(){
-  printf("Bad command line input:\n\t./fileCompressor [-R] [-b or -c or -d] <path or file> |codebook|\n");
-  exit(EXIT_FAILURE);
-  return 1;
+    printf("Bad command line input:\n\t./fileCompressor [-R] [-b or -c or -d] <path or file> |codebook|\n");
+    exit(EXIT_FAILURE);
+    return 1;
 }
 int improperName(char* name){
-  printf("Bad command line input:\n\t%s not a proper file or directory\n",name);
-  exit(EXIT_FAILURE);
-  return 1;
+    printf("Bad command line input:\n\t%s not a proper file or directory\n",name);
+    exit(EXIT_FAILURE);
+    return 1;
 }
