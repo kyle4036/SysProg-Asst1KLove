a994fde389db699f05dbfca7d4e147a7750c2d99
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-03 19:02:48 -0400

    fixing tabbing:Huffman.c

--- Huffman.c
+++ Huffman.c
@@ -16,118 +16,118 @@
 //char* flagToString(char* flag);
 
 int buildCodeBook(char* fileName, int fd){
-  printf("Building codebook for %s!\n", fileName);
+    printf("Building codebook for %s!\n", fileName);
 
-  lList* minHeap = readInData(fd);
-  return 0;
+    lList* minHeap = readInData(fd);
+    return 0;
 }
 
 lList* readInData(int fd){
-  int count;
-  char charTemp;
+    int count;
+    char charTemp;
 
-  char* spcPntr = malloc(sizeof(char)*strnlen(" ", 5));
-  CHECKMALLOC(spcPntr);
-  *spcPntr = " ";
+    char* spcPntr = malloc(sizeof(char)*strnlen(" ", 5));
+    CHECKMALLOC(spcPntr);
+    *spcPntr = " ";
 
-  char** stringTemp;
-  int stringLen = 2;
-  stringTemp = malloc(sizeof(char*));
-  CHECKMALLOC(stringTemp);
+    char** stringTemp;
+    int stringLen = 2;
+    stringTemp = malloc(sizeof(char*));
+    CHECKMALLOC(stringTemp);
 
-  lList* minHeap;
+    lList* minHeap;
 
-  count = read(fd,&charTemp,sizeof(char));
-  CHECKREAD(count);
-
-  minHeap = createList(spcPntr);
-
-  *stringTemp = malloc(sizeof(char*) * stringLen);
-  CHECKMALLOC(*stringTemp);
-  strncat(*stringTemp,&charTemp,1);
-
-  while(count != 0){
     count = read(fd,&charTemp,sizeof(char));
     CHECKREAD(count);
 
-    if(charTemp == ' '){
-      addToken(minHeap, spcPntr);
-      addToken(minHeap,*stringTemp);
+    minHeap = createList(spcPntr);
 
-      //free(*stringTemp);
-      stringTemp = malloc(sizeof(char*));
-      CHECKMALLOC(stringTemp);
+    *stringTemp = malloc(sizeof(char*) * stringLen);
+    CHECKMALLOC(*stringTemp);
+    strncat(*stringTemp,&charTemp,1);
 
-      stringLen = 2;
+    while(count != 0){
+        count = read(fd,&charTemp,sizeof(char));
+        CHECKREAD(count);
 
-      *stringTemp = malloc(sizeof(char*) * stringLen);
-      CHECKMALLOC(*stringTemp);
+        if(charTemp == ' '){
+            addToken(minHeap, spcPntr);
+            addToken(minHeap,*stringTemp);
 
-      count = read(fd,&charTemp,sizeof(char));
-      CHECKREAD(count);
-      strncat(*stringTemp,&charTemp,1);
+            //free(*stringTemp);
+            stringTemp = malloc(sizeof(char*));
+            CHECKMALLOC(stringTemp);
+
+            stringLen = 2;
+
+            *stringTemp = malloc(sizeof(char*) * stringLen);
+            CHECKMALLOC(*stringTemp);
+
+            count = read(fd,&charTemp,sizeof(char));
+            CHECKREAD(count);
+            strncat(*stringTemp,&charTemp,1);
+        }
+        else{
+            stringLen++;
+            catString(stringTemp,stringLen,charTemp);
+        }
     }
-    else{
-      stringLen++;
-      catString(stringTemp,stringLen,charTemp);
-    }
-  }
 }
 
 void catString(char** stringO,int stringLen,char charTemp){
-  char* stringTemp = malloc(sizeof(char)* stringLen);
-  CHECKMALLOC(stringTemp);
-  strncpy(stringTemp, *stringO,stringLen);
-  strncat(stringTemp, &charTemp,1);
-  free(*stringO);
-  *stringO = stringTemp;
+    char* stringTemp = malloc(sizeof(char)* stringLen);
+    CHECKMALLOC(stringTemp);
+    strncpy(stringTemp, *stringO,stringLen);
+    strncat(stringTemp, &charTemp,1);
+    free(*stringO);
+    *stringO = stringTemp;
 }
 
 int compress(char* fileName,char* codeBook){
-  printf("Compressing %s using %s codebook!\n",fileName, codeBook);
-  return 0;
+    printf("Compressing %s using %s codebook!\n",fileName, codeBook);
+    return 0;
 }
 
 int decompress(char* fileName,char* codeBook){
-  printf("Decompressing %s using %s codebook!\n",fileName, codeBook);
-  return 0;
+    printf("Decompressing %s using %s codebook!\n",fileName, codeBook);
+    return 0;
 }
 
 int recursive(char* pathName, char* flag, char* codeBook){
-  char* action = flagToString(flag);
-  printf("Recursively %s directory %s!\n",action,pathName);
-  free(action);
-  return 0;
+    char* action = flagToString(flag);
+    printf("Recursively %s directory %s!\n",action,pathName);
+    free(action);
+    return 0;
 }
 
 char* flagToString(char* flag){
-  int length;
+    int length;
 
-  char bString[] = "building codebook for";
-  char cString[] = "compressing";
-  char dString[] = "decompressing";
+    char bString[] = "building codebook for";
+    char cString[] = "compressing";
+    char dString[] = "decompressing";
 
-  if(strncmp(flag, "-b",3)){
-    length = strnlen(bString,30);
-    char* action = malloc(sizeof(char) * length);
-    strncpy(action,bString,length);
-    return action;
-  }
-  else if(strncmp(flag, "-c",3)){
-    length = strnlen(cString,30);
-    char* action = malloc(sizeof(char) * length);
-    strncpy(action,cString,length);
-    return action;
-  }
-  else if(strncmp(flag, "-d",3)){
-    length = strnlen(dString,30);
-    char* action = malloc(sizeof(char) * length);
-    strncpy(action,dString,length);
-    return action;
-  }
-  else{
-    printf("Error In flagToString():\n\t Bad flag.\n");
-    exit(EXIT_FAILURE);
-    //return (char*)"a";
-  }
+    if(strncmp(flag, "-b",3)){
+        length = strnlen(bString,30);
+        char* action = malloc(sizeof(char) * length);
+        strncpy(action,bString,length);
+        return action;
+    }
+    else if(strncmp(flag, "-c",3)){
+        length = strnlen(cString,30);
+        char* action = malloc(sizeof(char) * length);
+        strncpy(action,cString,length);
+        return action;
+    }
+    else if(strncmp(flag, "-d",3)){
+        length = strnlen(dString,30);
+        char* action = malloc(sizeof(char) * length);
+        strncpy(action,dString,length);
+        return action;
+    }
+    else{
+        printf("Error In flagToString():\n\t Bad flag.\n");
+        exit(EXIT_FAILURE);
+        //return (char*)"a";
+    }
 }
