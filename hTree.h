1b88906a55070c03b648ccfbb7fa97f75bcc73b3
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-04 12:41:37 -0400

    Update hTree.h

---
+++ hTree.h
@@ -0,0 +1,25 @@
+#include "lList.h"
+
+#ifndef HTREE_H
+#define HTREE_H
+
+    typedef struct h_node_{
+        int leaf;
+        int freq;
+        char* token;
+        struct h_node_* left;
+        struct h_node_* right;
+    } h_node;
+
+    typdef struct hTree_{
+        h_node* root;
+        int freq;
+    }
+
+    hTree* createTree(h_node* left, h_node* right);
+    void addToken(lList* data, hTree* tree);
+    hTree* combineTrees(hTree* left, hTree* right);
+
+    void freeTree(hTree* tree);
+
+#endif
