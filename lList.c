5b4653982fde39269c3d7e541945d52ce6107696
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-04-04 02:06:21 -0400

    Update lList.c

--- lList.c
+++ lList.c
@@ -76,7 +76,7 @@ void addToken(lList* data,char* token){
             while(next->next != NULL && temp->freq > next->freq)
                 next = next->next;//to bubble up to the correct freq val
 
-            if(temp->freq > data->head->freq)//if next is at the head value
+            if(temp->freq > (data->head).freq)//if next is at the head value
                 swap(temp,next,data);
             else
                 swap(temp,next->prev,data);
